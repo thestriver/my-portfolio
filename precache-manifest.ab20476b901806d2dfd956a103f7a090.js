289c1f6513039cdb17dea54f1521750c7b153900
thestriver <wannabeamumin@gmail.com>
2020-01-17 08:46:20 +0000

    Updates

---
+++ precache-manifest.ab20476b901806d2dfd956a103f7a090.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c107ff99b231f171654e132777028b",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "46c590ad1c55523d4558",
+    "url": "/my-portfolio/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6c271e4d5def07c2a6be",
+    "url": "/my-portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "46c590ad1c55523d4558",
+    "url": "/my-portfolio/static/js/2.9684d7e6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/my-portfolio/static/js/2.9684d7e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c271e4d5def07c2a6be",
+    "url": "/my-portfolio/static/js/main.5a330f3a.chunk.js"
+  },
+  {
+    "revision": "c76e595a7e7cbc144a48",
+    "url": "/my-portfolio/static/js/runtime-main.40ca06be.js"
+  },
+  {
+    "revision": "a181c4b2ab14b5057e1c4b2ace861659",
+    "url": "/my-portfolio/static/media/logo.a181c4b2.svg"
+  }
+]);
