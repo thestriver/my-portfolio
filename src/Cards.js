b0d0ce20153f9606e880c3983e9886a8442cd294
thestriver <wannabeamumin@gmail.com>
2020-01-26 14:39:02 +0000

    portfolio

--- src/Cards.js
+++ src/Cards.js
@@ -4,10 +4,14 @@ import React from 'react'
 function Cards(props) {
     
     return (
-        <div className="fl w-50 pa2">		
-            <img src={props.image} alt="portfolio-image"/>
-            <p>{props.title}</p>
-            <p>{props.description}</p>
+        <div className="fl w-50 grow">		
+            <a className="link black-70" href={props.gitlink}>
+                <img className="nb4" src={props.image} alt="portfolio-image"/>
+                <div className="tc nt2">
+                    <p className="f4 b">{props.title}</p>
+                    <p className="nt2">{props.description}</p> 
+                </div>
+            </a>
 		</div>
 
         
@@ -19,3 +23,8 @@ function Cards(props) {
 
 
 export default Cards;
+
+
+//tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5
+
+//f4 b nt2
--- src/Projects.js
+++ src/Projects.js
@@ -4,37 +4,50 @@ import Cards from './Cards'
 
 function Projects(props) {
 	return (
-		<div className="tc mb6">
+		<div className="tc cf">
+
 			<Cards
-				image="/portfolio_img/mwdigestfullb.png"				
-				title="MWD LANDING PAGE"
-				description="A landing page built with HTML, CSS and Bootstrap"
-			/>
-			<Cards
-				image="../portfolio_img/agritechfullb.jpg"			
-				title="AGTECH VENTURES WEBSITE"
-				description="A minimalist landing page built with HTML and CSS"
-			/>
-			<Cards
-				image="../portfolio_img/huddle2b.png"			
-				title="HUDDLE LANDING PAGE"
-				description="A landing page built with HTML, CSS"
-			/>
-			<Cards
-				image="../portfolio_img/huddlea.png"				
+				gitlink="https://github.com/thestriver/frontendprojects/tree/master/Landing_page_with_introduction"
+				image="./portfolio_img/huddlea.png"				
 				title="HUDDLE WELCOME PAGE"
-				description="A landing page built with HTML, CSS and Javascript"
+				description="HTML5, CSS3, and Javascript"
 			/>
+
 			<Cards
-				image="../portfolio_img/infochainfulla.jpg"				
-				title="INFOCHAIN I.T SOLUTIONS WEBSITE"
-				description="A landing page built with HTML, CSS and Bootstrap"
-			/>
-			<Cards
-				image="../portfolio_img/reactfull.png"				
+				gitlink="https://github.com/thestriver/robofriends"
+				image="./portfolio_img/reactfull.png"				
 				title="ROBOFRIENDS LANDING PAGE"
-				description="A landing page built with HTML, CSS and React JS"
+				description="HTML, CSS, React JS and API"
 			/>
+			<Cards
+				gitlink="https://github.com/thestriver/frontendprojects/tree/master/huddle_landing-page-with-alternating-feature-blocks"
+				image="./portfolio_img/huddle2b.png"			
+				title="HUDDLE LANDING PAGE"
+				description="HTML and CSS"
+			/>
+			
+
+			<Cards
+				gitlink="https://muslimsweekenddigest.com"
+				image="./portfolio_img/mwdigestfullb.png"				
+				title="MWD LANDING PAGE"
+				description="HTML and CSS(Bootstrap)"
+			/>
+			<Cards
+				gitlink="https://github.com/thestriver/AgTech-Ventures"
+				image="./portfolio_img/agritechfullb.jpg"			
+				title="AGTECH VENTURES WEBSITE"
+				description="HTML and CSS(Bootstrap)"
+			/>
+			
+			<Cards
+				gitlink="https://infochainng.com"
+				image="./portfolio_img/infochainfulla.jpg"				
+				title="INFOCHAIN I.T SOLUTIONS"
+				description="HTML and CSS(Bootstrap)"
+			/>
+			
+			
 		</div>
 
 
