9d594509f57404182eb471edced3119c04e7f5ca
thestriver <wannabeamumin@gmail.com>
2020-01-28 23:29:28 +0000

    styling

--- src/App.js
+++ src/App.js
@@ -20,10 +20,10 @@ function App() {
         <nav className="dt w-100 border-box pa1 ph6-ns">
           <img src={logo} className="dib w4 h4 br-100" alt="Mahmud"/>
           <div className="dtc v-mid w-75 tr">
-            <Link to="/projects" className="link dim dark-gray f4 f4-ns dib mr3 mr4-ns">Projects</Link>
-            <Link to="/contact" className="link dim dark-gray f4 f4-ns dib mr3 mr4-ns">Contact</Link>
-            <Route exact path="/projects" component={Projects} />
-            <Route exact path="/contact" component={Contact} />
+            {/* <Link to="" className="link dim dark-gray f4 f4-ns dib mr3 mr4-ns">Projects</Link>
+            <Link to="" className="link dim dark-gray f4 f4-ns dib mr3 mr4-ns">Contact</Link>
+            {/* <Route exact path="/projects" component={Projects} /> */}
+            {/* <Route exact path="/contact" component={Contact} /> */}
           </div>
         </nav>
 
