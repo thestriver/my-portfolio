ff0420c6b2ec11ec5ff126d6f1cc36567c8178a4
thestriver <wannabeamumin@gmail.com>
2020-01-29 00:38:06 +0000

    mail

--- src/Contact.js
+++ src/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
           <p className="f4">
           I'm always interested in exciting ideas and projects and it would be a pleasure to work with you. <br/> 
           Want to drop by and say hi? 
-          <a className="f4 fw6 db dark-blue no-underline underline-hover hover" href="mailto:hi@adeleyemahmud.com"> Send me a mail: hi[at]adeleyemahmud.com</a>
+          <a className="f4 fw6 db dark-blue no-underline underline-hover hover" href="mailto:mahmudadeleye@gmail.com"> Send me a mail: hi[at]adeleyemahmud.com</a>
           <a className="f4 fw6 db dark-blue no-underline underline-hover" href="https://linkedin.com/in/mahmudadeleye/"> or find me on LinkedIn.</a>
           </p>
         </div>
