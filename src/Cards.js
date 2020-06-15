3a74fa71009571be0e6ce80477d6b35984e6acde
thestriver <wannabeamumin@gmail.com>
2020-06-15 12:00:08 +0100

    dark mode

--- src/Cards.js
+++ src/Cards.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Cards(props) {
     
     return (
-        <div className="fl w-100 w-50-m w-50-l grow">		
+        <div className="fl w-100 w-100-m w-40-l grow ml5-l">		
             <a className="link black-70" href={props.gitlink}>
                 <img className="nb4" src={props.image} alt="portfolio"/>
                 <div className="tc nt2">
@@ -24,7 +24,3 @@ function Cards(props) {
 
 export default Cards;
 
-
-//tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5
-
-//f4 b nt2
