e5ae89cae98f8d33095b636d15a3a54f85f70209
thestriver <wannabeamumin@gmail.com>
2020-06-16 09:55:12 +0100

    darkmode

--- src/Intro.js
+++ src/Intro.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Intro() {
     return (
         <div className="pv5 f4 f2-ns measure center pr3 pl3 justify-center">
-          <h1 className="fw6 f1 fl w-100 black mt0 mb3 sans-serif">Hi! <span role="img"> 👋</span> It's a pleasure to have you here.</h1>
-          <p className="db lh-copy black serif fw1 mv0 f4 f3-m f2-l measure-wide monaco">I'm Mahmud Adeleye - a UK based 
+          <h1 className="fw6 f1 fl w-100 mt0 mb3 sans-serif">Hi! <span role="img"> 👋</span> It's a pleasure to have you here.</h1>
+          <p className="db lh-copy serif fw1 mv0 f4 f3-m f2-l measure-wide monaco">I'm Mahmud Adeleye - a UK based 
           software developer currently in an MSc. Computing Science program. Looking for opportunities to create awesome stuff 
           that matter. 
           </p>
