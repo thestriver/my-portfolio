ed2a5f3ae65800fc988a91336eed28b8e7461c52
thestriver <wannabeamumin@gmail.com>
2021-04-18 13:34:43 -0600

    index

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Cards(props) {
     
     return (
-        <div className="fl  w-100 w-100-m w-40-l grow ml5-l">		
+        <div className="fl w-100 w-100-m w-40-l grow ml5-l">		
             <a className="link" href={props.gitlink}>
                 <img className="nb4" src={props.image} alt="portfolio"/>
                 <div className="tc nt5">
